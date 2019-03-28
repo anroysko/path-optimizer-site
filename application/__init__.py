0d558405a7577f1e36ba1552d04d3a6dce4d10a8
Antti Roeyskoe <aroysko@paivola.fi>
2019-03-28 21:28:22 +0200

    Write logging in

--- application/__init__.py
+++ application/__init__.py
@@ -1,13 +1,37 @@
+#Import Flask
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
+#Import the database
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///maps.db"
 app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
+#Site-specific functionality
 from application import views
 from application.maps import models
 from application.maps import views
+from application.auth import models
+from application.auth import views
 
+#Logging in
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(user_id)
+
+#Create tables if needed
 db.create_all()
--- application/maps/views.py
+++ application/maps/views.py
@@ -1,26 +1,29 @@
 from flask import render_template, request, redirect, url_for
 from application import app, db
 from application.maps.models import Map
+from application.maps.forms import NewMapForm, EditMapForm
 from sqlalchemy import update
 
 @app.route("/maps/<map_id>", methods=["GET"])
 def maps_view(map_id):
-	return render_template("maps/map.html", found_map = Map.query.get(map_id))
+	return render_template("maps/map.html", found_map = Map.query.get(map_id), form = EditMapForm())
 
 @app.route("/maps/new/")
 def maps_form():
-	return render_template("maps/new.html")
+	return render_template("maps/new.html", form = NewMapForm())
 
 @app.route("/maps/", methods=["POST"])
 def maps_create():
-	m = Map(request.form.get("name"))
+	form = NewMapForm(request.form)
+	m = Map(form.name.data)
 	db.session().add(m)
 	db.session().commit()
 	return redirect("/maps/" + str(m.id))
 
 @app.route("/maps/edit/", methods=["POST"])
 def maps_edit():
-	m = Map.query.get(int(request.form.get("map_id")))
-	m.name = request.form.get("name")
+	form = EditMapForm(request.form)
+	m = Map.query.get(int(form.map_id.data))
+	m.name = form.name.data
 	db.session().commit()
 	return redirect("/maps/" + str(m.id))
