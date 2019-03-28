0740a2067190a8e41aa9a2e0c1a7524f24d4c9e2
Antti Roeyskoe <aroysko@paivola.fi>
2019-03-28 23:44:48 +0200

    Add registration

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField
+from wtforms import PasswordField, StringField, validators
   
 class LoginForm(FlaskForm):
 	username = StringField("Username")
+	password = PasswordField("Password")
+
+	class Meta:
+		csrf = False
+
+class RegisterForm(FlaskForm):
+	username = StringField("Username", [validators.Length(min=1)])
 	password = PasswordField("Password")
 
 	class Meta:
--- application/auth/views.py
+++ application/auth/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 
-from application import app
+from application import app, db
 from application.auth.models import User
-from application.auth.forms import LoginForm
+from application.auth.forms import LoginForm, RegisterForm
+from sqlalchemy import update
 
 @app.route("/auth/login", methods = ["GET", "POST"])
 def auth_login():
@@ -11,13 +12,34 @@ def auth_login():
 		return render_template("auth/loginform.html", form = LoginForm())
 
 	form = LoginForm(request.form)
-	# Validation?
+	if not form.validate():
+		return render_template("auth/loginform.html", form = form)
 
 	user = User.query.filter_by(username=form.username.data, password=form.password.data).first()
 	if not user:
 		return render_template("auth/loginform.html", form = form, error = "No such username/password")
 
 	login_user(user)
+	return redirect(url_for("index"))
+
+@app.route("/auth/register", methods = ["GET", "POST"])
+def auth_register():
+	if request.method == "GET":
+		return render_template("auth/registerform.html", form = RegisterForm())
+
+	form = RegisterForm(request.form)
+	if not form.validate():
+		return render_template("auth/registerform.html", form = form)
+
+	user = User.query.filter_by(username=form.username.data).first()
+	if user:
+		return render_template("auth/registerform.html", form = form, error = "Please choose an unique username")
+	
+	user = User(form.username.data, form.password.data)
+	db.session().add(user)
+	db.session().commit()
+	login_user(user)
+
 	return redirect(url_for("index"))
 
 @app.route("/auth/logout")
