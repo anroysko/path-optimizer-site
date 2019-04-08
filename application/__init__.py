15fcffef1a1b313a2ff5dac9e02310b0e8f85242
Antti Roeyskoe <aroysko@paivola.fi>
2019-04-08 03:19:24 +0300

    Add aggregate query

--- application/__init__.py
+++ application/__init__.py
@@ -34,7 +34,6 @@ login_manager.init_app(app)
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality"
 
-
 @login_manager.user_loader
 def load_user(user_id):
 	return User.query.get(user_id)
--- application/views.py
+++ application/views.py
@@ -8,8 +8,13 @@ from sqlalchemy import update
 @app.route("/")
 def index():
 	public_maps = db.engine.execute("SELECT * FROM map WHERE private=False")
+	aggregate_maps = db.engine.execute("SELECT * FROM Map"
+		" LEFT JOIN Hex ON Hex.map_id = Map.id"
+		" WHERE (Map.private = 0 OR Map.account_id = :i)"
+		" GROUP BY Map.id"
+		" HAVING COUNT(Hex.id) = 0", i = current_user.get_id())
 	if current_user.get_id():
 		user_maps = db.engine.execute("SELECT * FROM map WHERE account_id=" + str(current_user.get_id()))
-		return render_template("index.html", public_maps=public_maps, user_maps=user_maps)
+		return render_template("index.html", public_maps=public_maps, user_maps=user_maps, aggregate_maps=aggregate_maps)
 	else:
-		return render_template("index.html", public_maps=public_maps)
+		return render_template("index.html", public_maps=public_maps, aggregate_maps=aggregate_maps)
