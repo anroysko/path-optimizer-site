48ab959de42eb4a73976a004a1b244143d5a2cbd
Antti Roeyskoe <aroysko@paivola.fi>
2019-04-08 04:12:31 +0300

    Fix issue with aggregate query

--- application/views.py
+++ application/views.py
@@ -8,8 +8,8 @@ from sqlalchemy import update
 @app.route("/")
 def index():
 	public_maps = db.engine.execute("SELECT * FROM map WHERE private=False")
-	aggregate_maps = db.engine.execute("SELECT * FROM Map"
-		" LEFT JOIN Hex ON Hex.map_id = Map.id"
+	aggregate_maps = db.engine.execute("SELECT Map.*, Hex.map_id FROM"
+		" MAP LEFT JOIN Hex ON Hex.map_id = Map.id"
 		" WHERE (Map.private = 0 OR Map.account_id = :i)"
 		" GROUP BY Map.id"
 		" HAVING COUNT(Hex.id) = 0", i = current_user.get_id())
