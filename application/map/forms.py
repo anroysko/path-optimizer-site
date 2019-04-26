6beb450e169a7d03e5eb32f789414e62d9bbdac0
Antti Roeyskoe <aroysko@paivola.fi>
2019-04-26 05:42:51 +0300

    Make map page more responsive, and allow larger maps

--- application/map/forms.py
+++ application/map/forms.py
@@ -3,16 +3,16 @@ from wtforms import StringField, IntegerField, BooleanField, validators
 
 class NewMapForm(FlaskForm):
 	name = StringField("Map name", [validators.Length(min=1)])
-	width = IntegerField("Map width", [validators.DataRequired(), validators.NumberRange(1, 100)])
-	height = IntegerField("Map height", [validators.DataRequired(), validators.NumberRange(1, 100)])
+	width = IntegerField("Map width", [validators.DataRequired(), validators.NumberRange(1, 20)])
+	height = IntegerField("Map height", [validators.DataRequired(), validators.NumberRange(1, 20)])
 
 	class Meta:
 		csrf = False
 
 class EditMapForm(FlaskForm):
 	name = StringField("New name", [validators.Length(min=1)])
-	width = IntegerField("Map width", [validators.DataRequired(), validators.NumberRange(1, 100)])
-	height = IntegerField("Map height", [validators.DataRequired(), validators.NumberRange(1, 100)])
+	width = IntegerField("Map width", [validators.DataRequired(), validators.NumberRange(1, 20)])
+	height = IntegerField("Map height", [validators.DataRequired(), validators.NumberRange(1, 20)])
 
 	class Meta:
 		csrf = False
