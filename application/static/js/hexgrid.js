ee21700208f580bd9520aa4dbe6b3d62c42fb61c
Antti Roeyskoe <aroysko@paivola.fi>
2019-05-03 23:46:20 +0300

    Add path optimization. It currently only counts number of paths necessary

--- application/static/js/hexgrid.js
+++ application/static/js/hexgrid.js
@@ -39,18 +39,6 @@ function setHexState(x, y, i, v, state) {
 	}
 }
 
-function getCounts(w, h, state) {
-	var city_cou = 0;
-	var road_cou = 0;
-	var empty_cou = w * h + Math.floor(h / 2);
-	for (var key in state) {
-		if (state[key] == "1") ++city_cou;
-		else if (state[key] == "2") ++road_cou;
-	}
-	empty_cou -= city_cou + road_cou;
-	return [empty_cou, city_cou, road_cou];
-}
-
 function hexClick(x, y) {
 	var dts = document.getElementById("hex-grid").dataset;
 	const w = parseInt(dts.w);
@@ -90,14 +78,208 @@ function clearPaths() {
 	enableButton(getSaveButton());
 }
 
+function solve(w, h, data_array) {
+	// Let k be the number of cities, and n=w*h Then this is O(3^k n + 2^k n^2)
+	var k = 0;
+	var city_xs = [];
+	var city_ys = [];
+	var city_is = [];
+	for (var y = 0; y < h; ++y) {
+		for (var x = 0; x <= w; ++x) {
+			const i = x + y * (w+1);
+			if (data_array[i] == 1) {
+				++k;
+				city_xs.push(x);
+				city_ys.push(y);
+				city_is.push(i);
+			}
+		}
+	}
+	if (k == 1) return data_array; // No changes required
+
+	var edges = [];
+	for (var y = 0; y < h; ++y) {
+		for (var x = 0; x <= w; ++x) {
+			const i = x + y * (w+1);
+
+			var targets = [];
+			if ((y % 2 == 1) || (x < w)) {
+				if (y % 2 == 0) {
+					if (x > 0) {
+						targets.push(i-1); // left
+					}
+					if (y > 0) {
+						targets.push(i - (w+1)); // up-left
+						targets.push(i - (w+1) + 1); // up-right
+					}
+					if (x+1 < w) {
+						targets.push(i + 1); // right
+					}
+					if (y+1 < h) {
+						targets.push(i + (w+1) + 1); // down-right
+						targets.push(i + (w+1)); // down-left
+					}
+				} else {
+					if (x > 0) {
+						targets.push(i-1); // left
+					}
+					if (y > 0) {
+						if (x > 0) {
+							targets.push(i - (w+1) - 1); // up-left
+						}
+						if (x < w) {
+							targets.push(i - (w+1)); // up-right
+						}
+					}
+					if (x+1 < w+1) {
+						targets.push(i + 1); // right
+					}
+					if (y+1 < h) {
+						if (x < w) {
+							targets.push(i + (w+1)); // down-right
+						}
+						if (x > 0) {
+							targets.push(i + (w+1) - 1); // down-left
+						}
+					}
+				}
+			}
+			edges.push(targets);
+		}
+	}
+
+	const mm = 1 << (k-1);
+	const mi = (w+1)*h;
+	const inf = 1000000000;
+
+	var dp = [[]]; // dummy unused first dimension
+	for (var mask = 1; mask < mm; ++mask) {
+		// O(3^k n) part
+		var dists = []
+		for (var y = 0; y < h; ++y) {
+			for (var x = 0; x <= w; ++x) {
+				const i = x + y * (w+1);
+				var d = inf;
+				for (var submask = mask; submask != 0; submask = (submask - 1) & mask) {
+					const invmask = mask ^ submask;
+					if (invmask == 0) {
+						continue;
+					}
+					d = Math.min(d, dp[submask][i] + dp[invmask][i]);
+				}
+				if ((data_array[i] == 0) && (d != inf)) {
+					d -= 1; // Refund one payment of the node
+				}
+				dists.push(d);
+			}
+		}
+		
+		if ((mask & (mask - 1)) == 0) {
+			for (var j = 0; j < k-1; ++j) {
+				if (mask & (1 << j)) {
+					dists[city_is[j]] = 0;
+				}
+			}
+		}
+
+		// O(2^k n^2) part
+		var que = [];
+		for (var d = 0; d <= mi; ++d) {
+			que.push([]);
+		}
+		for (var i = 0; i <= mi; ++i) {
+			if (dists[i] <= mi) {
+				que[dists[i]].push(i);
+			}
+		}
+		for (var d = 0; d <= mi; ++d) {
+			for (var j = 0; j < que[d].length; ++j) {
+				// Check if we have already handled this
+				const i = que[d][j];
+				if (dists[i] < d) {
+					continue;
+				}
+
+				// Update distances
+				for (var ti = 0; ti < edges[i].length; ++ti) {
+					const t = edges[i][ti];
+					const c = d + (data_array[t] == 0 ? 1 : 0);
+					if (dists[t] > c) {
+						dists[t] = c;
+						que[c].push(t);
+					}
+				}
+			}
+		}
+
+		// Save the DP and continue
+		dp.push(dists);
+	}
+	alert("min added roads: " + dp[mm-1][city_is[k-1]]);
+
+	// Build the solution
+	/*
+	var state = []
+	for (var i = 0; i < mi; ++i) {
+		state.push(data_array[i]);
+	}
+	for (
+
+	console.log("min added roads: " + dp[mm-1][city_is[k-1]]);
+	*/
+	return;
+}
+
 function optimize() {
 	var dts = document.getElementById("hex-grid").dataset;
 	const w = parseInt(dts.w);
 	const h = parseInt(dts.h);
 
 	var state = getHexgridJsonState();
-	const ini_cou = getCounts(w, h, state);
-	alert("This button doesn't currently do anything.\nEnjoy some stats instead!\nEmpty hexes: " + ini_cou[0] + "\nCities: " + ini_cou[1] + "\nRoads: " + ini_cou[2]);
+	var data_array = []
+
+	var city_cou = 0;
+
+	for (var y = 0; y < h; ++y) {
+		for (var x = 0; x <= w; ++x) {
+			var i = x + y * (w+1);
+			if (state[i] === "1") {
+				data_array.push(1); // City
+				++city_cou;
+			} else if (state[i] === "2") {
+				data_array.push(2); // Road
+			} else {
+				data_array.push(0); // Empty
+			}
+		}
+	}
+	if (city_cou < 2) return;
+
+	// Find the optimal steiner tree
+	solve(w, h, data_array);
+
+	/*
+	var best = solve(w, h, data_array);
+
+	// Change into the best state
+	for (var y = 0; y < h; ++y) {
+		for (var x = 0; x <= w; ++x) {
+			var i = x + y * (w+1);
+			if (best[i] != data_array[i]) {
+				if (best[i] == 0) {
+					setHexState(x, y, i, undefined, state);
+				} else if (best[i] == 1) {
+					setHexState(x, y, i, "1", state);
+				} else if (best[i] == 2) {
+					setHexState(x, y, i, "2", state);
+				}
+			}
+		}
+	}
+
+	setHexgridJsonState(state);
+	enableButton(getSaveButton());
+	*/
 }
 
 function getSaveButton() {
